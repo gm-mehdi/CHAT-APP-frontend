ae41036d846108b4584a2d0c35c731c3099d77dd
gm-mehdi <mehdi.elgoummadi@gmail.com>
2024-03-14 20:05:58 +0000

    added login and signup form pages

---
+++ src/pages/signup/GenderCheckBox.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const GenderCheckBox = () => {
+    return (
+        <div className='flex mt-2'>
+            <div className="form-control">
+                <label className="cursor-pointer label gap-2">
+                    <span className="label-text">Male</span>
+                    <input type="checkbox" defaultChecked className="checkbox checkbox-info" />
+                </label>
+            </div>
+            <div className="form-control">
+                <label className="cursor-pointer label gap-2">
+                    <span className="label-text">Female</span>
+                    <input type="checkbox" defaultChecked className="checkbox checkbox-info" />
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default GenderCheckBox
